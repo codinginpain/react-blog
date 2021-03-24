ad72b40fac02312265452fc329551f181e937793
JUNGKEE KIM <kee@JUNGKEEui-MacBookPro.local>
2021-03-24 18:08:16 +0900

    before postman installing

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 //after mongoose install
 const mongoose = require("mongoose");
+//after body-parser & cookieparser install
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 mongoose.connect('mongodb+srv://kee:blog123@react-blog-clone.asf65.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {useNewUrlParser: true}).then(() => console.log('mongoDB connected'))
                             .catch(err => {console.error(err)});
 
+app.use(bodyPaser.urlencoded({ extended : true })); //queryString 사용을 위해
+app.use(bodyPaser.json()); // json
+app.use(cookieParser()); // cookie
 
 app.get('/', (req, res)=> {
     res.send("Hello World");
